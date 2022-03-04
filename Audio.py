9234e8f8bc79193f1a75aa9d7b7cb5fd80478e0e
Kenneth Corrales <kcorrales33@gmail.com>
2022-03-04 14:06:23 -0600

    Primer acercamiento al trabajo, grabacion y graficacion
    realizado por Erick

---
+++ Audio.py
@@ -0,0 +1,15 @@
+import sounddevice as sd 
+from scipy.io.wavfile import write 
+import wavio as wv 
+  
+freq = 44100  
+duration = 5
+sd.default.samplerate = freq
+sd.default.channels = 2
+recording = sd.rec(int(duration * freq))
+  
+sd.wait() 
+  
+write("recording0.wav", freq, recording) 
+  
+wv.write("recording1.wav", recording, freq, sampwidth=2)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Redes_IC7602-2022
+
+## Como Ejecutar
+Este trabajo utiliza varias bibliotecas que son necesarias de instalar para hacerlo podemos seguir los siguientes pasos:
+- Instalar 
