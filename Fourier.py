cb6ca3e6fbd524f9f39543e9943f34821014b8be
Erick <erickbf22@estudiantec.cr>
2022-03-07 18:13:55 -0600

    Permite grabar audio y capacidad de procesarlo

---
+++ Fourier.py
@@ -0,0 +1,36 @@
+import winsound
+import scipy.io.wavfile as waves 
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.fftpack as fourier
+from pandas.core.common import flatten
+filename = "recording1.wav"
+print(filename)
+winsound.PlaySound(filename,winsound.SND_FILENAME)
+
+
+Fs,data = waves.read(filename)
+data= list(flatten(data))
+Audio_m = data[:len(data)]
+Largo = len(Audio_m)
+Ts= 0.001
+n = Ts*np.arange(0,Largo)
+
+
+
+fig,ax = plt.subplots()
+plt.plot(n,Audio_m)
+plt.xlabel('Tiempo')
+plt.ylabel('Audio')
+
+
+gk = fourier.fft(Audio_m) #Aplicamos Fourier sobre el vector donde guardamos el audio
+M_gk = abs(gk) #Sacamos el valor absoluto luego de aplicar Fourier
+M_gk = M_gk[0:Largo//2] #Tomamos la mitad porque son 2 iguales y solo nos interesa tomar una 
+
+F= (Fs/Largo)*np.arange(0,Largo//2)
+fig,bx= plt.subplots()
+plt.plot(F,M_gk)
+plt.xlabel('Frecuencia', fontsize='14')
+plt.ylabel('Amplitud FFT', fontsize='14')
+plt.show()
